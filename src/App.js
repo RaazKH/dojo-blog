9bca557e809643ab82f7c10d9f6e8dcc74be45cc
RaazKH <raazkhoshnood@gmail.com>
2021-04-10 16:37:08 +0300

    Updated code for lesson 8

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = "Dog salad";
-  const chickens = 50;
-  const link = "https://google.com";
 
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>I have {chickens} chickens!</p>
-        <a href={link}>Google</a>
         <Home />
       </div>
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,13 @@
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello' + name, e.target);
+    const handleClick = () => {
+        
     }
 
     return ( 
         <div className="home">
-            <h2>homepage</h2>
+            <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('Mario', e)}>Click me again</button>
         </div>
      );
 }
