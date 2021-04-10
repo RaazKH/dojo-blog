0be0da70c531fb37fcc4678e0665045468f9f442
RaazKH <raazkhoshnood@gmail.com>
2021-04-10 16:27:54 +0300

    anonymous function  for button 2

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, ninjas');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + name);
+    }
+
     return ( 
         <div className="home">
             <h2>homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => {
+                console.log('hello')
+                }
+            }>Click me again</button>
         </div>
      );
 }
