dbe839d79fc966351d16cb9549932e3b36974b5e
RaazKH <serxtdweia9r@gmail.com>
2021-04-12 17:07:34 +0300

    cleared Home.js for lesson 16

--- src/Home.js
+++ src/Home.js
@@ -8,24 +8,18 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
-    const [name, setName] = useState('mario');
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
-    useEffect(() => { // this function runs for every render
+    useEffect(() => {
         console.log('use effect ran');
-        console.log(name);
-    }, [name]); // name added as dependency so only runs at name statechange
-                // leave brackets empty to make function run once on page load
+    }, []);
 
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-           <button onClick={() => setName('luigi')}>change name</button>
-           <p>{name}</p>
         </div>
      );
 }
