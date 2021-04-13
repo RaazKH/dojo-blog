4c562cccec8952b3f6b0d2249c1206be37d25462
RaazKH <serxtdweia9r@gmail.com>
2021-04-13 17:26:03 +0300

    Added custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,10 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null); // initial data is null so conditional evaluator is needed below
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
 
-    useEffect(() => {
-        // setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) { // throw error for catch below
-                        throw Error('Could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setIsPending(false);
-                    setBlogs(data) // this wont cause an infinite loop b/c it only runs at the start
-                    setError(null);
-                })
-                .catch(err => {
-                    setError(err.message);
-                    setIsPending(false);
-                    console.log(err.message);
-                })
-        // }, 1000);
-    }, []);
-
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+    
     return ( 
         <div className="home">
             { error && <div>{error}</div>}
---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => { // must start hook name with "use"
+    const [data, setData] = useState(null); // initial data is null so conditional evaluator is needed below
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    
+    useEffect(() => {
+        // setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if(!res.ok) { // throw error for catch below
+                        throw Error('Could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setIsPending(false);
+                    setData(data) // this wont cause an infinite loop b/c it only runs at the start
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message);
+                    setIsPending(false);
+                    console.log(err.message);
+                })
+        // }, 1000);
+    }, [url]);
+
+    return { data, isPending, error }
+}
+export default useFetch;
