80d01229a9fab16817c7c3d10ae2fb2ada5460c3
RaazKH <serxtdweia9r@gmail.com>
2021-04-14 13:23:52 +0300

    made BlogDetails.js fetch blogs from db

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,28 +8,26 @@ const useFetch = (url) => { // must start hook name with "use"
     useEffect(() => {
         const abortCont = new AbortController();
 
-        setTimeout(() => {
-            fetch(url, { signal: abortCont.signal })
-                .then(res => {
-                    if(!res.ok) { // throw error for catch below
-                        throw Error('Could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
+        fetch(url, { signal: abortCont.signal })
+            .then(res => {
+                if(!res.ok) { // throw error for catch below
+                    throw Error('Could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setIsPending(false);
+                setData(data) // this wont cause an infinite loop b/c it only runs at the start
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                } else {
+                    setError(err.message);
                     setIsPending(false);
-                    setData(data) // this wont cause an infinite loop b/c it only runs at the start
-                    setError(null);
-                })
-                .catch(err => {
-                    if (err.name === 'AbortError') {
-                        console.log('fetch aborted');
-                    } else {
-                        setError(err.message);
-                        setIsPending(false);
-                    }
-                })
-        }, 1000);
+                }
+            })
 
         return () => abortCont.abort();
     }, [url]);
