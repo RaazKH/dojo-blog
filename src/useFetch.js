a84e7d7759140fdfef14eeef48520fc7331580d6
RaazKH <serxtdweia9r@gmail.com>
2021-04-14 10:06:41 +0300

    Added abort controller to useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => { // must start hook name with "use"
     const [error, setError] = useState(null);
     
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     if(!res.ok) { // throw error for catch below
                         throw Error('Could not fetch the data for that resource');
@@ -20,11 +22,16 @@ const useFetch = (url) => { // must start hook name with "use"
                     setError(null);
                 })
                 .catch(err => {
-                    setError(err.message);
-                    setIsPending(false);
-                    console.log(err.message);
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted');
+                    } else {
+                        setError(err.message);
+                        setIsPending(false);
+                    }
                 })
         }, 1000);
+
+        return () => abortCont.abort();
     }, [url]);
 
     return { data, isPending, error }
